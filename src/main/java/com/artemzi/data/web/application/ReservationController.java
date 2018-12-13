cece2800f9522a6f8138b61844ae24008ced6d94
Artem Z <arzinoviev@gmail.com>
2018-12-13 18:08:09 +0300

    Add get by date implementation

--- src/main/java/com/artemzi/data/web/application/ReservationController.java
+++ src/main/java/com/artemzi/data/web/application/ReservationController.java
@@ -1,15 +1,46 @@
 package com.artemzi.data.web.application;
 
+import com.artemzi.domains.RoomReservation;
+import com.artemzi.services.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/reservations")
 public class ReservationController {
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+    private ReservationService reservationService;
+
+    @Autowired
+    public void getReservationService(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
 
     @GetMapping
-    public String getReservations() {
+    public String getReservations(@RequestParam(value = "date", required = false) String dateString,
+                                  Model model) {
+        Date date;
+        if (null != dateString) {
+            try {
+                date = DATE_FORMAT.parse(dateString);
+            } catch (ParseException e) {
+                date = new Date();
+            }
+        } else {
+            date = new Date();
+        }
+        List<RoomReservation> roomReservationList = this.reservationService.getRoomReservationForDate(date);
+        model.addAttribute("roomReservations", roomReservationList);
         return "reservations";
     }
 
